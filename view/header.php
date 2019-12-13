c99faea189d7eefe65b47a7932b0b44e36c18413
DougGoncalves <doug.goncalvesf@gmail.com>
2019-12-13 20:07:18 -0300

    deploy

---
+++ view/header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html ng-app="shop">
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width:device-width, initial-scale=1">
+		<title>Orlando City</title>
+		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="lib/owl.carousel/owl-carousel/owl.carousel.css">
+		<link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="lib/raty/lib/jquery.raty.css">
+		<link rel="stylesheet" href="css/orlando.css">
+		<link rel="stylesheet" href="css/orlando-mobile.css">
+
+		<script src="lib/angularjs/angular.min.js"></script>
+
+	</head>
+	<body>
+
+		<header>
+			
+			<div id="menu-mobile-mask" class="visible-xs"></div>
+
+			<div id="menu-mobile" class="visible-xs">
+				
+				<ul class="list-unstyled">
+					<li><a href="videos">Videos</a></li>
+					<li><a href="#">Tickets</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Schedule</a></li>
+				</ul>
+
+				<div class="bar-close">
+					<button type="button" class="btn btn-close"><i class="fa fa-close"></i></button>
+				</div>
+
+			</div>
+			
+			<div class="container">
+				<a href="index.php">
+				<img id="logotipo" src="img/orlando-logo.png" alt="Logotipo">
+				</a>
+			</div>
+
+			<div class="header-black">
+				
+				<div class="container">
+
+					<input type="search" id="input-search-mobile" class="visible-xs" placeholder="search...">
+				
+					<button id="btn-bars" type="button"><i class="fa fa-bars"></i></button>
+					<button id="btn-search" type="button"><i class="fa fa-search"></i></button>
+
+					<ul class="pull-right">
+						<li class="club-01"><a href="https://www.chicagofirefc.com"></a></li>
+						<li class="club-02"><a href="https://www.lafc.com"></a></li>
+						<li class="club-03"><a href="https://www.coloradorapids.com"></a></li>
+						<li class="club-04"><a href="https://www.columbuscrewsc.com"></a></li>
+						<li class="club-05"><a href="https://www.dcunited.com"></a></li>
+						<li class="club-06"><a href="https://www.fcdallas.com"></a></li>
+						<li class="club-07"><a href="https://www.houstondynamo.com"></a></li>
+						<li class="club-08"><a href="https://www.lagalaxy.com"></a></li>
+						<li class="club-09"><a href="https://www.impactmontreal.com/en"></a></li>
+						<li class="club-10"><a href="https://www.revolutionsoccer.net"></a></li>
+						<li class="club-11"><a href="https://www.newyorkredbulls.com"></a></li>
+						<li class="club-12"><a href="https://www.nycfc.com"></a></li>
+						<li class="club-13"><a href="https://www.philadelphiaunion.com"></a></li>
+						<li class="club-14"><a href="https://www.timbers.com"></a></li>
+						<li class="club-15"><a href="https://www.rsl.com"></a></li>
+						<li class="club-16"><a href="https://www.sjearthquakes.com"></a></li>
+					</ul>
+
+				</div>
+
+			</div>
+
+			<div class="container">
+				
+				<div class="row">
+					
+					<nav id="menu" class="pull-right">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="videos">Videos</a></li>
+							<li><a href="#">Tickets</a></li>
+							<li><a href="#">News</a></li>
+							<li><a href="#">Schedule</a></li>
+							<li class="search">
+								<div class="input-group">
+							      <input type="search" placeholder="search" id="input-search">
+							      <span class="input-group-btn">
+							        <button type="button"><i class="fa fa-search"></i></button>
+							      </span>
+							    </div><!-- /input-group -->
+							</li>
+						</ul>
+					</nav>
+
+				</div>
+
+			</div>			
+
+		</header>
